5da44254960e5687b3e0fd35c0b8bc1e82dda9e2
Tiramisu <135003824+Se7enseads@users.noreply.github.com>
2025-03-09 06:31:13 +0300

    :pencil2: fix table name typo
    - Change Admin Password

--- OnlineVotingSystem.api/Data/OnlineVotingSystemContext.cs
+++ OnlineVotingSystem.api/Data/OnlineVotingSystemContext.cs
@@ -7,7 +7,7 @@ public class OnlineVotingSystemContext(DbContextOptions<OnlineVotingSystemContex
 {
     public DbSet<Candidate> Candidates => Set<Candidate>();
     public DbSet<Election> Elections => Set<Election>();
-    public DbSet<ElectionPosition> ElectionsPositions => Set<ElectionPosition>();
+    public DbSet<ElectionPosition> ElectionPositions => Set<ElectionPosition>();
     public DbSet<Position> Positions => Set<Position>();
     public DbSet<User> Users => Set<User>();
     public DbSet<Vote> Votes => Set<Vote>();
@@ -21,7 +21,7 @@ public class OnlineVotingSystemContext(DbContextOptions<OnlineVotingSystemContex
                 Name = "Admin",
                 NationalId = 10000001,
                 Email = "admin@system.com",
-                Password = "$2a$11$PlHURX9wlCUp.yY038iuOu6u9k7oMIZ2O6vAwiaRsJtMzYlC8NWby", // Securely hashed password
+                Password = "$2a$11$McDzAqqk04VkMTWMQmZnw.DR.uCHl/wj23tAKPZBJNSygN2koP8gK", // AdminPassword!1234
                 IsAdmin = true,
                 CreatedAt = new DateTime(2024, 2, 11, 12, 0, 0, DateTimeKind.Utc)
             }
